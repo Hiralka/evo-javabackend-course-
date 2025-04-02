679226b40872ba5c10a7a8bf9a44049c86079737
Hiralka <artempro7@yandex.ru>
2025-04-02 17:07:37 +0300

    проект не открывался из-за 4.18

---
+++ 4.20 Задание 18. Реализовать вывод списка сообщений у конкретного пользователя./application2/src/main/java/com/hiraru/application2/dto/Person.java
@@ -0,0 +1,54 @@
+package com.hiraru.application2.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String firstname;
+    private String surname;
+    private String lastname;
+    private LocalDate birthday;
+    
+    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    private List<Message> messages = new ArrayList<>();
+
+
+    // Конструкторы
+    public Person() {}
+
+    public Person(String firstname, String surname, String lastname, LocalDate birthday) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.lastname = lastname;
+        this.birthday = birthday;
+        this.messages = new ArrayList<>();
+    }
+
+    // Геттеры и сеттеры
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getFirstname() { return firstname; }
+    public void setFirstname(String firstname) { this.firstname = firstname; }
+    public String getSurname() { return surname; }
+    public void setSurname(String surname) { this.surname = surname; }
+    public String getLastname() { return lastname; }
+    public void setLastname(String lastname) { this.lastname = lastname; }
+    public LocalDate getBirthday() { return birthday; }
+    public void setBirthday(LocalDate birthday) { this.birthday = birthday; }
+    public List<Message> getMessages() { return messages == null ? new ArrayList<>() : messages; }
+    public void setMessages(List<Message> messages) { this.messages = messages; }
+
+    public void addMessage(Message message) {
+        message.setPerson(this);
+        messages.add(message);
+    }
+}
+
