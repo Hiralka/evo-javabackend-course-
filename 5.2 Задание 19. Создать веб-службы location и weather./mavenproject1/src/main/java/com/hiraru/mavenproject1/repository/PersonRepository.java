484e3b7307a3bcd64c5466b295abbaca5e3c288b
Hiralka <artempro7@yandex.ru>
2025-04-02 21:57:32 +0300

    тесты

--- 5.2 Задание 19. Создать веб-службы location и weather./mavenproject1/src/main/java/com/hiraru/mavenproject1/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hiraru.mavenproject1.repository;
-
-import com.hiraru.mavenproject1.dto.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
