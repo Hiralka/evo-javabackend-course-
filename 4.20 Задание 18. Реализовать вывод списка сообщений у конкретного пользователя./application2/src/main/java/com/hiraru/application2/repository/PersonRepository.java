55cf4de86764179e3eeac3b3a3a2f5a78ef1f79d
Hiralka <artempro7@yandex.ru>
2025-04-02 21:54:19 +0300

    тесты

--- 4.20 Задание 18. Реализовать вывод списка сообщений у конкретного пользователя./application2/src/main/java/com/hiraru/application2/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hiraru.application2.repository;
-
-import com.hiraru.application2.dto.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person, Integer> {
-}
